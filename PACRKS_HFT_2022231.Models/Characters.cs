c3cb689c3e148581a76512d361be10a94db72950
berzerg02 <pocsainorbertjanos@gmail.com>
2022-11-17 19:04:16 +0100

    Characters class has been corrected

--- PACRKS_HFT_2022231.Models/Characters.cs
+++ PACRKS_HFT_2022231.Models/Characters.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,9 +11,12 @@ namespace PACRKS_HFT_2022231.Models
     public class Characters
     {
         [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int CharacterId { get; set; }
         public string Name { get; set; }
-        public List<string> Skins { get; set; }
+        public string Skin { get; set; }
         public string Type { get; set; }
-        
+        public virtual ICollection<Player> Players { get; set; }
+
     }
 }
