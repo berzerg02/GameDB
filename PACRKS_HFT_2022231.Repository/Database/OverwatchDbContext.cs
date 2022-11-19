a4a95dfec5c7f95ea5f68a8d778607bcff27f1c8
berzerg02 <pocsainorbertjanos@gmail.com>
2022-11-19 17:31:26 +0100

    Extended the database.

--- PACRKS_HFT_2022231.Repository/Database/OverwatchDbContext.cs
+++ PACRKS_HFT_2022231.Repository/Database/OverwatchDbContext.cs
@@ -52,12 +52,20 @@ namespace PACRKS_HFT_2022231.Repository
                     new Characters() {Name = "Mercy", Skin = "Amber", Type = "Support", CharacterId = 5},
                     new Characters() {Name = "Echo", Skin = "Durlan", Type = "Dps", CharacterId = 6},
                     new Characters() {Name = "Widowmaker", Skin = "Dawn", Type = "Dps", CharacterId = 7},
+                    new Characters() {Name = "Bastion", Skin = "Sky", Type = "Dps", CharacterId = 9},
+                    new Characters() {Name = "Sigma", Skin = "Earth", Type = "Tank", CharacterId = 10},
+                    new Characters() {Name = "Sombra", Skin = "Grass", Type = "Dps", CharacterId = 11},
+                    new Characters() {Name = "Winston", Skin = "Sunset", Type = "Tank", CharacterId = 12},
+                    new Characters() {Name = "Kariko", Skin = "Crystallic", Type = "Support", CharacterId = 13},
+                    new Characters() {Name = "Lucio", Skin = "Blacked", Type = "Support", CharacterId = 14},
+                    new Characters() {Name = "Tracer", Skin = "Mono", Type = "Dps", CharacterId = 15},
+                    new Characters() {Name = "Ashe", Skin = "Scientific", Type = "Dps", CharacterId = 16},
 
                 };
 
             var players = new Player[]
                 {
-                    new Player() {Username = "SpooderMan", CharacterId = 1, MatchId = 1, Rank = "Plastic", StatId = 0, PlayerId = 8 },
+                    new Player() {Username = "SpooderMan", CharacterId = 1, MatchId = 1, Rank = "Plastic", StatId = 8, PlayerId = 8 },
                     new Player() {Username = "Karvesz", CharacterId = 5, MatchId = 1, Rank = "Potato", StatId = 1, PlayerId = 1},
                     new Player() {Username = "HatHelo", CharacterId = 8, MatchId = 1, Rank = "Grandmaster", StatId = 2, PlayerId = 2},
                     new Player() {Username = "Rpq", CharacterId = 4, MatchId = 1, Rank = "Bronze", StatId = 3, PlayerId = 3},
@@ -65,6 +73,15 @@ namespace PACRKS_HFT_2022231.Repository
                     new Player() {Username = "iBronn", CharacterId = 7, MatchId = 1, Rank = "Gold", StatId = 5, PlayerId = 5},
                     new Player() {Username = "Wolverine", CharacterId = 2, MatchId = 1, Rank = "Platinum", StatId = 6, PlayerId = 6},
                     new Player() {Username = "Nesquik", CharacterId = 6, MatchId = 1, Rank = "Diamond", StatId = 7, PlayerId = 7},
+
+                    new Player() {Username = "ManDingo", CharacterId = 9, MatchId = 2, Rank = "Diamond", StatId = 9, PlayerId = 9},
+                    new Player() {Username = "HatHelo", CharacterId = 10, MatchId = 2, Rank = "Gold", StatId = 10, PlayerId = 10},
+                    new Player() {Username = "Noroi", CharacterId = 11, MatchId = 2, Rank = "Platinum", StatId = 11, PlayerId = 11},
+                    new Player() {Username = "Raven", CharacterId = 12, MatchId = 2, Rank = "Immortal", StatId = 12, PlayerId = 12},
+                    new Player() {Username = "Beatlewhochewshgum", CharacterId = 13, MatchId = 2, Rank = "Grandmaster", StatId = 13, PlayerId = 13},
+                    new Player() {Username = "Grandpachairgaming", CharacterId = 14, MatchId = 2, Rank = "Diamond", StatId = 14, PlayerId = 14},
+                    new Player() {Username = "Spiderwit3legs", CharacterId = 15, MatchId = 2, Rank = "Diamond", StatId = 15, PlayerId = 15},
+                    new Player() {Username = "Dinodog08", CharacterId = 16, MatchId = 2, Rank = "Diamond", StatId = 16, PlayerId = 16},
 
                 };
 
@@ -77,14 +94,24 @@ namespace PACRKS_HFT_2022231.Repository
                     new Stats() {Kills = 25, Deaths = 20, ShotsFired = 43242, ShotsConnected = 1231, TimePlayed = 30f, StatId = 4},
                     new Stats() {Kills = 21, Deaths = 10, ShotsFired = 213123, ShotsConnected = 1221, TimePlayed = 31f, StatId = 5},
                     new Stats() {Kills = 18, Deaths = 4, ShotsFired = 12312, ShotsConnected = 3422, TimePlayed = 28.6f, StatId = 6},
-                    new Stats() {Kills = 15, Deaths = 8, ShotsFired = 564564, ShotsConnected = 4533, TimePlayed = 29.7f, StatId = 7}
+                    new Stats() {Kills = 15, Deaths = 8, ShotsFired = 564564, ShotsConnected = 4533, TimePlayed = 29.7f, StatId = 7},
+
+                    new Stats() {Kills = 24, Deaths = 8, ShotsFired = 321231, ShotsConnected = 12312, TimePlayed = 22, StatId = 9},
+                    new Stats() {Kills = 26, Deaths = 10, ShotsFired = 123123, ShotsConnected = 3122, TimePlayed = 23, StatId = 10},
+                    new Stats() {Kills = 13, Deaths = 30, ShotsFired = 41231, ShotsConnected = 32131, TimePlayed = 24, StatId = 11},
+                    new Stats() {Kills = 5, Deaths = 8, ShotsFired = 42313, ShotsConnected = 31231, TimePlayed = 25, StatId = 12},
+                    new Stats() {Kills = 32, Deaths = 3, ShotsFired = 213123, ShotsConnected = 123123, TimePlayed = 26, StatId = 13},
+                    new Stats() {Kills = 15, Deaths = 5, ShotsFired = 523324, ShotsConnected = 12312, TimePlayed = 27, StatId = 14},
+                    new Stats() {Kills = 17, Deaths = 20, ShotsFired = 1231231, ShotsConnected = 3123, TimePlayed = 28, StatId = 15},
+                    new Stats() {Kills = 20, Deaths = 30, ShotsFired = 123342, ShotsConnected = 12312, TimePlayed = 29.3f, StatId = 16}
 
 
                 };
             ;
             var match = new Match[]
                 {
-                    new Match() {Length = 35f, Map = "Dorado", MatchId = 1, Type = "Ranked"}
+                    new Match() {Length = 35f, Map = "Dorado", MatchId = 1, Type = "Ranked"},
+                    new Match() {Length = 30f, Map = "Havana", MatchId = 2, Type = "Casual"}
 
                 };
 
