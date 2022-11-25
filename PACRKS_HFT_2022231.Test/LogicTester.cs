37aae434d492d0732663cb63eda8365201f87a2f
berzerg02 <pocsainorbertjanos@gmail.com>
2022-11-25 03:06:49 +0100

    Editing

--- PACRKS_HFT_2022231.Test/LogicTester.cs
+++ PACRKS_HFT_2022231.Test/LogicTester.cs
@@ -60,54 +60,10 @@ namespace PACRKS_HFT_2022231.Test
             mockCharacterRepo = new Mock<IRepository<Characters>>();
             mockMatchesRepo = new Mock<IRepository<Matches>>(); 
 
-
-
-
-
-            //mockStatsRepo = new Mock<IRepository<Stats>>();
-            //mockStatsRepo.Setup(m => m.ReadAll()).Returns(new List<Stats>()
-            //{
-            //    new Stats() {Kills = 100, Deaths = 2, ShotsFired = 100000, ShotsConnected = 100, TimePlayed = 30f, StatId = 8},
-            //    new Stats() {Kills = 20, Deaths = 10, ShotsFired = 100, ShotsConnected = 100, TimePlayed = 29f, StatId = 1},
-            //    new Stats() {Kills = 1, Deaths = 50, ShotsFired = 23142, ShotsConnected = 2312, TimePlayed = 26f, StatId = 2},
-            //    new Stats() {Kills = 30, Deaths = 6, ShotsFired = 120012, ShotsConnected = 4221, TimePlayed = 28f, StatId = 3},
-            //    new Stats() {Kills = 25, Deaths = 20, ShotsFired = 43242, ShotsConnected = 1231, TimePlayed = 30f, StatId = 4},
-            //    new Stats() {Kills = 21, Deaths = 10, ShotsFired = 213123, ShotsConnected = 1221, TimePlayed = 31f, StatId = 5},
-            //    new Stats() {Kills = 18, Deaths = 4, ShotsFired = 12312, ShotsConnected = 3422, TimePlayed = 28.6f, StatId = 6},
-            //    new Stats() {Kills = 15, Deaths = 8, ShotsFired = 564564, ShotsConnected = 4533, TimePlayed = 29.7f, StatId = 7},
-            //}.AsQueryable());
-
-            //mockPlayerRepo = new Mock<IRepository<Player>>();
-            //mockPlayerRepo.Setup(x => x.ReadAll()).Returns(new List<Player>()
-            //{
-            //    new Player() {Username = "SpooderMan", CharacterId = 1, MatchId = 1, Rank = "Plastic", StatId = 8, PlayerId = 8 },
-            //    new Player() {Username = "Karvesz", CharacterId = 5, MatchId = 1, Rank = "Potato", StatId = 1, PlayerId = 1},
-            //    new Player() {Username = "HatHelo", CharacterId = 8, MatchId = 1, Rank = "Grandmaster", StatId = 2, PlayerId = 2},
-            //    new Player() {Username = "Rpq", CharacterId = 4, MatchId = 1, Rank = "Bronze", StatId = 3, PlayerId = 3},
-            //    new Player() {Username = "Bundi", CharacterId = 3, MatchId = 1, Rank = "Silver", StatId = 4, PlayerId = 4},
-            //    new Player() {Username = "iBronn", CharacterId = 7, MatchId = 1, Rank = "Gold", StatId = 5, PlayerId = 5},
-            //    new Player() {Username = "Wolverine", CharacterId = 2, MatchId = 1, Rank = "Platinum", StatId = 6, PlayerId = 6},
-            //    new Player() {Username = "Nesquik", CharacterId = 6, MatchId = 1, Rank = "Diamond", StatId = 7, PlayerId = 7}
-
-            //}.AsQueryable());
-
-            //mockMatchesRepo = new Mock<IRepository<Matches>>();
-            //mockMatchesRepo.Setup(x => x.ReadAll()).Returns(new List<Matches>()
-            //{
-            //    new Matches() {Length = 35f, Map = "Dorado", MatchId = 1, Type = "Ranked"},
-            //    new Matches() {Length = 30f, Map = "Havana", MatchId = 2, Type = "Casual"}
-            //}.AsQueryable());
-
-
-
-
             logic = new StatsLogic(mockStatsRepo.Object);
             playerlogic = new PlayerLogic(mockPlayerRepo.Object);
             characterlogic = new CharactersLogic(mockCharacterRepo.Object);
             matchlogic = new MatchLogic(mockMatchesRepo.Object);
-
-
-
         }
 
         [Test]
@@ -160,7 +116,7 @@ namespace PACRKS_HFT_2022231.Test
         [Test]
         public void Create2()
         {
-            Player sample = new Player(1, "Wasman", "PLastic", 1, 1, 1);
+            Player sample = new Player(1, "Wasman", "Plastic", 1, 1, 1);
 
             playerlogic.Create(sample);
             mockPlayerRepo.Verify(x => x.Create(sample), Times.Once);
